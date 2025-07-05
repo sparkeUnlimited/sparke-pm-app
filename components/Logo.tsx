b0155e671a09c821b804eb845b28bb56d64fdb35
Spark-E Unlimited <rmaxwell@sparkeunlimited.ca>
2025-07-04 23:10:48 -0400

    Fix Logo component return and expand README

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 export default function Logo() {
-  <Image
-    src="/assets/img/larger_icon.png"
-    alt="logo"
-    width={100}
-    height={100}
-    style={{ objectFit: "contain" }}
-  />;
+  return (
+    <Image
+      src="/assets/img/larger_icon.png"
+      alt="logo"
+      width={100}
+      height={100}
+      style={{ objectFit: "contain" }}
+    />
+  );
 }
